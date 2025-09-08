111e8861c06326c8f9bc92181fb77751c2168da7
Kaio Gomes <kaio.correa@aethra.com.br>
2025-09-08 13:39:51 -0300

    update

--- frontend/src/app/views/tarefas/layout/tarefas.component.ts
+++ frontend/src/app/views/tarefas/layout/tarefas.component.ts
@@ -1,11 +1,59 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
 
-import { CardBodyComponent, CardComponent, CardHeaderComponent, ColComponent, RowComponent } from '@coreui/angular';
+import {
+  CardBodyComponent,
+  CardComponent,
+  CardHeaderComponent,
+  ColComponent,
+  RowComponent,
+  ButtonDirective,
+  ColDirective,
+  FormControlDirective,
+  FormDirective,
+  FormLabelDirective,
+  GutterDirective,
+  RowDirective,
+  BorderDirective,
+  CardTextDirective,
+  CardTitleDirective
+  
+} from '@coreui/angular';
 
+import { DocsComponentsComponent } from '@docs-components/public-api';
 @Component({
   selector: 'app-tarefas',
-  imports: [RowComponent, ColComponent,  CardComponent, CardHeaderComponent, CardBodyComponent],
+  imports: [
+    RowComponent,
+    ColComponent,
+    CardComponent,
+    CardHeaderComponent,
+    CardBodyComponent,
+    DocsComponentsComponent,
+    ButtonDirective,
+    ColDirective,
+    FormControlDirective,
+    FormDirective,
+    FormLabelDirective,
+    GutterDirective,
+    RowDirective,
+    BorderDirective,
+    CardTextDirective,
+    CardTitleDirective,
+    NgFor
+  ],
   templateUrl: './tarefas.component.html',
   styleUrl: './tarefas.component.scss'
 })
-export class TarefasComponent {}
+export class TarefasComponent { 
+  colors = [
+    { color: 'primary', textColor: 'primary' },
+    { color: 'secondary', textColor: 'secondary' },
+    { color: 'success', textColor: 'success' },
+    { color: 'danger', textColor: 'danger' },
+    { color: 'warning', textColor: 'warning' },
+    { color: 'info', textColor: 'info' },
+    { color: 'light', textColor: '' },
+    { color: 'dark', textColor: '' }
+  ];
+}

1bdffeefe51cc4fdd9e5ed0486c963d4f5acb263
Kaio Gomes <1369299@sga.pucminas.br>
2025-09-10 23:00:05 -0300

    Adicionando Swagger

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,16 @@ builder.Services.AddCors(options =>
                         .AllowAnyMethod());
 });
 
+builder.Services.AddSwaggerGen(options =>
+{
+    // Configuração do Swagger para adicionar uma documentação da API
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Keevo API",
+        Version = "v1",
+        Description = "Documentação da API Keevo"
+    });
+});
 
 builder.Services.AddControllers();
 
@@ -20,7 +31,19 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<KeevoDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+app.UseSwagger();
+
+// Habilita o Swagger UI para acessar a documentação
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Keevo API V1");
+    options.RoutePrefix = "swagger";  // Define a URL de acesso ao Swagger UI como a raiz do projeto
+});
+
 app.UseCors("AllowAngular");
+
+
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
